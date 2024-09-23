7343b38b76e0f7d883768ac08b4b8e533c896135
Clemence <clemence.bcq@gmail.com~>
2024-09-23 17:47:07 +0200

    modification des insertions

--- dataPersonnes.sql
+++ dataPersonnes.sql
@@ -1,63 +1,50 @@
-DROP TABLE IF EXISTS `myTable`;
 
-CREATE TABLE `myTable` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `NOMPO` varchar(255) default NULL,
-  `PRENOMPO` varchar(255) default NULL,
-  `EMAIL` varchar(255) default NULL,
-  `DDNPO` varchar(255),
-  `alphanumeric` mediumint default NULL,
-  `ADRESSE` varchar(255) default NULL,
-  `VILLE` varchar(255),
-  `PORTABLE` varchar(100) default NULL,
-  PRIMARY KEY (`id`)
-) AUTO_INCREMENT=1;
 
-INSERT INTO `myTable` (`NOMPO`,`PRENOMPO`,`EMAIL`,`DDNPO`,`alphanumeric`,`ADRESSE`,`VILLE`,`PORTABLE`)
+INSERT INTO PERSONNE (NOMPO, PRENOMPO , EMAIL , DDNPO , alphanumeric , ADRESSE , VILLE , PORTABLE )
 VALUES
-  ("Perry","Macon","in@icloud.org","2010/59/18",20,"Ap #731-3409 Eu Road","Leoben","03 36 78 33 08"),
+  ("Perry","Macon","in@icloud.org","2010/09/18",20,"Ap #731-3409 Eu Road","Leoben","03 36 78 33 08"),
   ("Suarez","Cecilia","vivamus.non@google.edu","2000/11/14",29,"621 Etiam Av.","Södertälje","05 05 21 22 81"),
-  ("Morris","Brielle","semper@yahoo.ca","2019/26/05",31,"819-3025 Lacus. Rd.","Redlands","06 82 05 38 68"),
+  ("Morris","Brielle","semper@yahoo.ca","2019/06/05",31,"819-3025 Lacus. Rd.","Redlands","06 82 05 38 68"),
   ("Mccarty","Stuart","adipiscing.lobortis.risus@outlook.couk","2021/15/23",45,"2671 Tincidunt Avenue","Sarreguemines","03 63 57 85 20"),
-  ("Marsh","Brielle","faucibus.orci@icloud.org","2014/58/18",49,"P.O. Box 753, 1151 Cursus Rd.","Delhi","06 31 23 50 66"),
-  ("Davenport","Raymond","ante.dictum@hotmail.com","2013/21/25",23,"471-5249 Ligula Rd.","Gwangyang","07 43 74 15 75"),
+  ("Marsh","Brielle","faucibus.orci@icloud.org","2014/08/18",49,"P.O. Box 753, 1151 Cursus Rd.","Delhi","06 31 23 50 66"),
+  ("Davenport","Raymond","ante.dictum@hotmail.com","2013/01/25",23,"471-5249 Ligula Rd.","Gwangyang","07 43 74 15 75"),
   ("Cruz","Eaton","laoreet@outlook.net","2008/09/09",38,"5649 Vitae Rd.","Tabuk","06 20 43 68 42"),
-  ("Franklin","Nicole","congue@google.couk","2007/45/20",33,"P.O. Box 495, 4960 Curabitur St.","Jeongeup","03 08 68 41 41"),
-  ("Barrett","Clinton","interdum@icloud.edu","2022/23/08",45,"Ap #651-718 Sed St.","Melilla","04 93 55 61 23"),
-  ("Houston","Basia","penatibus@aol.edu","2013/28/03",32,"888-3365 Neque. St.","Kleinmachnow","09 31 64 22 54");
-INSERT INTO `myTable` (`NOMPO`,`PRENOMPO`,`EMAIL`,`DDNPO`,`alphanumeric`,`ADRESSE`,`VILLE`,`PORTABLE`)
+  ("Franklin","Nicole","congue@google.couk","2007/05/20",33,"P.O. Box 495, 4960 Curabitur St.","Jeongeup","03 08 68 41 41"),
+  ("Barrett","Clinton","interdum@icloud.edu","2022/03/08",45,"Ap #651-718 Sed St.","Melilla","04 93 55 61 23"),
+  ("Houston","Basia","penatibus@aol.edu","2013/08/03",32,"888-3365 Neque. St.","Kleinmachnow","09 31 64 22 54");
+INSERT INTO  myTable  ( NOMPO , PRENOMPO , EMAIL , DDNPO , alphanumeric , ADRESSE , VILLE , PORTABLE )
 VALUES
-  ("Harrell","Nathaniel","ac.ipsum.phasellus@outlook.edu","2019/55/30",31,"724-8442 Egestas. Av.","Gjøvik","04 46 67 12 86"),
-  ("Vega","Channing","dolor.vitae.dolor@outlook.com","2024/52/07",38,"718-4464 Vivamus Rd.","Leoben","05 41 43 37 16"),
-  ("Montoya","Dante","suspendisse.sed@yahoo.couk","2011/23/28",40,"P.O. Box 801, 2753 Ornare Av.","Gignod","08 38 08 00 68"),
-  ("Horn","Camille","habitant.morbi.tristique@protonmail.couk","2007/14/21",25,"796-2339 Aliquam St.","Mexico City","04 66 43 25 18"),
+  ("Harrell","Nathaniel","ac.ipsum.phasellus@outlook.edu","2019/05/30",31,"724-8442 Egestas. Av.","Gjøvik","04 46 67 12 86"),
+  ("Vega","Channing","dolor.vitae.dolor@outlook.com","2024/02/07",38,"718-4464 Vivamus Rd.","Leoben","05 41 43 37 16"),
+  ("Montoya","Dante","suspendisse.sed@yahoo.couk","2011/03/28",40,"P.O. Box 801, 2753 Ornare Av.","Gignod","08 38 08 00 68"),
+  ("Horn","Camille","habitant.morbi.tristique@protonmail.couk","2007/04/21",25,"796-2339 Aliquam St.","Mexico City","04 66 43 25 18"),
   ("King","Cassady","rutrum@google.couk","2006/01/04",25,"163-2249 Diam Ave","Barranca","03 67 04 22 81"),
-  ("Haley","Jana","dolor.quisque@outlook.couk","2021/41/23",26,"P.O. Box 611, 7785 Tincidunt Av.","Bad Oldesloe","08 37 44 87 63"),
+  ("Haley","Jana","dolor.quisque@outlook.couk","2021/01/23",26,"P.O. Box 611, 7785 Tincidunt Av.","Bad Oldesloe","08 37 44 87 63"),
   ("Hewitt","Hyatt","massa@aol.ca","2002/01/17",23,"P.O. Box 431, 858 Nulla Rd.","Radom","08 52 88 33 80"),
-  ("Bonner","Joel","non@protonmail.org","2002/17/21",21,"188-4693 Nam St.","Moelv","04 71 54 71 35"),
-  ("Burgess","Rashad","in.aliquet@icloud.net","2002/51/28",43,"Ap #384-5918 Nulla Avenue","Adelaide","08 10 42 27 83"),
-  ("Stark","Serena","varius.et@protonmail.net","2020/47/22",39,"Ap #791-731 Placerat. Ave","Veenendaal","06 45 75 54 12");
-INSERT INTO `myTable` (`NOMPO`,`PRENOMPO`,`EMAIL`,`DDNPO`,`alphanumeric`,`ADRESSE`,`VILLE`,`PORTABLE`)
+  ("Bonner","Joel","non@protonmail.org","2002/07/21",21,"188-4693 Nam St.","Moelv","04 71 54 71 35"),
+  ("Burgess","Rashad","in.aliquet@icloud.net","2002/01/28",43,"Ap #384-5918 Nulla Avenue","Adelaide","08 10 42 27 83"),
+  ("Stark","Serena","varius.et@protonmail.net","2020/07/22",39,"Ap #791-731 Placerat. Ave","Veenendaal","06 45 75 54 12");
+INSERT INTO  myTable  ( NOMPO , PRENOMPO , EMAIL , DDNPO , alphanumeric , ADRESSE , VILLE , PORTABLE )
 VALUES
   ("Payne","Chanda","erat.neque@protonmail.net","2022/02/22",37,"5828 Vel, Av.","Oaxaca","02 40 44 84 36"),
-  ("Franklin","Gil","taciti.sociosqu@outlook.edu","2003/46/13",37,"P.O. Box 739, 3227 Eleifend, St.","Pioneer","09 66 19 08 11"),
-  ("Sharp","Stuart","tincidunt@hotmail.ca","2004/32/08",44,"2867 Varius Av.","Lim Chu Kang","04 47 15 16 64"),
-  ("Rowe","Tara","vel.est.tempor@yahoo.org","2004/36/23",41,"Ap #813-6790 Imperdiet Road","Pirque","02 11 94 65 65"),
-  ("Alexander","Hiroko","ridiculus@icloud.couk","2015/15/26",42,"Ap #606-3538 Venenatis St.","Maubeuge","03 85 65 96 04"),
-  ("Flynn","Teegan","sagittis.semper@aol.ca","2007/45/11",31,"Ap #956-5843 Sem, Street","Pioneer","02 68 66 11 82"),
-  ("Lara","Hilda","sit.amet@yahoo.edu","2011/37/18",26,"P.O. Box 100, 3902 Fermentum St.","Santa Rosa de Cabal","07 76 38 56 83"),
+  ("Franklin","Gil","taciti.sociosqu@outlook.edu","2003/06/13",37,"P.O. Box 739, 3227 Eleifend, St.","Pioneer","09 66 19 08 11"),
+  ("Sharp","Stuart","tincidunt@hotmail.ca","2004/12/08",44,"2867 Varius Av.","Lim Chu Kang","04 47 15 16 64"),
+  ("Rowe","Tara","vel.est.tempor@yahoo.org","2004/06/23",41,"Ap #813-6790 Imperdiet Road","Pirque","02 11 94 65 65"),
+  ("Alexander","Hiroko","ridiculus@icloud.couk","2015/12/26",42,"Ap #606-3538 Venenatis St.","Maubeuge","03 85 65 96 04"),
+  ("Flynn","Teegan","sagittis.semper@aol.ca","2007/11/11",31,"Ap #956-5843 Sem, Street","Pioneer","02 68 66 11 82"),
+  ("Lara","Hilda","sit.amet@yahoo.edu","2011/07/18",26,"P.O. Box 100, 3902 Fermentum St.","Santa Rosa de Cabal","07 76 38 56 83"),
   ("Sims","Brandon","vestibulum@aol.com","2016/10/13",36,"P.O. Box 650, 882 Cursus Avenue","Perpignan","04 63 33 22 86"),
-  ("Alexander","Daria","nunc@outlook.ca","2021/20/23",41,"Ap #509-1030 Pellentesque Av.","Telford","02 48 27 88 53"),
+  ("Alexander","Daria","nunc@outlook.ca","2021/10/23",41,"Ap #509-1030 Pellentesque Av.","Telford","02 48 27 88 53"),
   ("Perez","Prescott","cursus.nunc@outlook.ca","2000/07/16",31,"625-8947 Est. Av.","Merzig","04 84 82 49 30");
-INSERT INTO `myTable` (`NOMPO`,`PRENOMPO`,`EMAIL`,`DDNPO`,`alphanumeric`,`ADRESSE`,`VILLE`,`PORTABLE`)
+INSERT INTO  myTable  ( NOMPO , PRENOMPO , EMAIL , DDNPO , alphanumeric , ADRESSE ,VILLE,PORTABLE)
 VALUES
   ("Blackwell","Kennedy","odio.aliquam.vulputate@google.edu","2003/08/15",39,"Ap #985-6107 Sem St.","Oaxaca","01 88 53 42 11"),
-  ("Butler","Tiger","in.ornare@aol.com","2008/58/09",28,"Ap #762-5007 Felis. Street","Hamburg","04 15 37 42 61"),
-  ("York","David","tellus.aenean@aol.net","2010/41/08",46,"551-6219 Donec Road","Mata de Plátano","05 35 44 61 37"),
-  ("Castillo","Kameko","luctus@icloud.ca","2000/28/09",35,"471-6917 Per St.","Chancay","02 31 14 83 09"),
+  ("Butler","Tiger","in.ornare@aol.com","2008/08/09",28,"Ap #762-5007 Felis. Street","Hamburg","04 15 37 42 61"),
+  ("York","David","tellus.aenean@aol.net","2010/01/08",46,"551-6219 Donec Road","Mata de Plátano","05 35 44 61 37"),
+  ("Castillo","Kameko","luctus@icloud.ca","2000/02/09",35,"471-6917 Per St.","Chancay","02 31 14 83 09"),
   ("Burnett","Gisela","ut.sagittis@aol.ca","2007/04/20",31,"Ap #678-6021 Nunc Street","Puno","05 49 27 77 53"),
-  ("Craft","Castor","nunc.interdum@aol.ca","2010/14/01",22,"Ap #194-2936 Risus. St.","Piedecuesta","05 77 48 22 77"),
-  ("Cunningham","Guy","integer@outlook.couk","2000/20/17",22,"694-4152 Nulla Ave","Guarulhos","07 13 67 08 22"),
-  ("Harris","Graiden","aliquam.ultrices@icloud.edu","2013/27/17",21,"8259 Sed Rd.","Cherkasy","04 35 48 85 24");
+  ("Craft","Castor","nunc.interdum@aol.ca","2010/03/01",22,"Ap #194-2936 Risus. St.","Piedecuesta","05 77 48 22 77"),
+  ("Cunningham","Guy","integer@outlook.couk","2000/07/17",22,"694-4152 Nulla Ave","Guarulhos","07 13 67 08 22"),
+  ("Harris","Graiden","aliquam.ultrices@icloud.edu","2013/07/17",21,"8259 Sed Rd.","Cherkasy","04 35 48 85 24");
 
 
