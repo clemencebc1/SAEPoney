3d68d9c5652719864c51635dc9950e6cae7f037c
clemence <clemence.bcq@gmail.com>
2024-09-28 21:56:17 +0200

    majout d'insertions et modification du script de creation

--- dataPersonnes.sql
+++ dataPersonnes.sql
@@ -2,47 +2,52 @@
 
 INSERT INTO PERSONNE (NOMPO, PRENOMPO, EMAIL, DDNPER, POIDS, ADRESSE, PORTABLE)
 VALUES
-  ("Perry","Macon","in@icloud.org","2010/09/18",20,"Ap #731-3409 Eu Road Orléans","03 36 78 33 08"),
-  ("Suarez","Cecilia","vivamus.non@google.edu","2000/01/14",29,"621 Etiam Av. Orléans","05 05 21 22 81"),
-  ("Morris","Brielle","semper@yahoo.ca","2019/06/05",31,"819-3025 Lacus. Rd. Orléans","06 82 05 38 68"),
-  ("Mccarty","Stuart","adipiscing.lobortis.risus@outlook.couk","2021/11/23",45,"2671 Tincidunt Avenue Orléans","03 63 57 85 20"),
-  ("Marsh","Brielle","faucibus.orci@icloud.org","2014/08/18",49,"P.O. Box 753, 1151 Cursus Rd. Orléans","06 31 23 50 66"),
-  ("Davenport","Raymond","ante.dictum@hotmail.com","2013/01/25",23,"471-5249 Ligula Rd. Orléans","07 43 74 15 75"),
-  ("Cruz","Eaton","laoreet@outlook.net","2008/09/09",38,"5649 Vitae Rd. Orléans","06 20 43 68 42"),
-  ("Franklin","Nicole","congue@google.couk","2007/05/20",33,"P.O. Box 495, 4960 Curabitur St. Orléans","03 08 68 41 41"),
-  ("Barrett","Clinton","interdum@icloud.edu","2022/03/08",45,"Ap #651-718 Sed St. Orléans","04 93 55 61 23"),
-  ("Houston","Basia","penatibus@aol.edu","2013/08/03",32,"888-3365 Neque. St. Orléans","09 31 64 22 54");
+  ("Perry","Macon","in@icloud.org",STR_TO_DATE("2010/09/18", "%Y/%m/%d"),20,"Ap #731-3409 Eu Road Orléans",03 36 78 33 08),
+  ("Suarez","Cecilia","vivamus.non@google.edu",STR_TO_DATE("2000/01/14", "%Y/%m/%d"),29,"621 Etiam Av. Orléans",05 05 21 22 81),
+  ("Morris","Brielle","semper@yahoo.ca",STR_TO_DATE("2019/06/05", "%Y/%m/%d"),31,"819-3025 Lacus. Rd. Orléans",06 82 05 38 68),
+  ("Mccarty","Stuart","adipiscing.lobortis.risus@outlook.couk",STR_TO_DATE("2021/11/23", "%Y/%m/%d"),45,"2671 Tincidunt Avenue Orléans",03 63 57 85 20),
+  ("Marsh","Brielle","faucibus.orci@icloud.org",STR_TO_DATE("2014/08/18", "%Y/%m/%d"),49,"P.O. Box 753, 1151 Cursus Rd. Orléans",06 31 23 50 66),
+  ("Davenport","Raymond","ante.dictum@hotmail.com",STR_TO_DATE("2013/01/25", "%Y/%m/%d"),23,"471-5249 Ligula Rd. Orléans",07 43 74 15 75),
+  ("Cruz","Eaton","laoreet@outlook.net",STR_TO_DATE("2008/09/09", "%Y/%m/%d"),38,"5649 Vitae Rd. Orléans",06 20 43 68 42),
+  ("Franklin","Nicole","congue@google.couk",STR_TO_DATE("2007/05/20", "%Y/%m/%d"),33,"P.O. Box 495, 4960 Curabitur St. Orléans",03 08 68 41 41),
+  ("Barrett","Clinton","interdum@icloud.edu",STR_TO_DATE("2022/03/08", "%Y/%m/%d"),45,"Ap #651-718 Sed St. Orléans",04 93 55 61 23),
+  ("Houston","Basia","penatibus@aol.edu",STR_TO_DATE("2013/08/03", "%Y/%m/%d"),32,"888-3365 Neque. St. Orléans",09 31 64 22 54),
 
-  ("Harrell","Nathaniel","ac.ipsum.phasellus@outlook.edu","2019/05/30",31,"724-8442 Egestas. Av. Orléans","04 46 67 12 86"),
-  ("Vega","Channing","dolor.vitae.dolor@outlook.com","2024/02/07",38,"718-4464 Vivamus Rd. Orléans","05 41 43 37 16"),
-  ("Montoya","Dante","suspendisse.sed@yahoo.couk","2011/03/28",40,"P.O. Box 801, 2753 Ornare Av. Orléans","08 38 08 00 68"),
-  ("Horn","Camille","habitant.morbi.tristique@protonmail.couk","2007/04/21",25,"796-2339 Aliquam St. Orléans","04 66 43 25 18"),
-  ("King","Cassady","rutrum@google.couk","2006/01/04",25,"163-2249 Diam Ave Orléans","03 67 04 22 81"),
-  ("Haley","Jana","dolor.quisque@outlook.couk","2021/01/23",26,"P.O. Box 611, 7785 Tincidunt Av. Orléans","08 37 44 87 63"),
-  ("Hewitt","Hyatt","massa@aol.ca","2002/01/17",23,"P.O. Box 431, 858 Nulla Rd. Orléans","08 52 88 33 80"),
-  ("Bonner","Joel","non@protonmail.org","2002/07/21",21,"188-4693 Nam St. Orléans","04 71 54 71 35"),
-  ("Burgess","Rashad","in.aliquet@icloud.net","2002/11/28",43,"Ap #384-5918 Nulla Avenue Orléans","08 10 42 27 83"),
-  ("Stark","Serena","varius.et@protonmail.net","2020/07/22",39,"Ap #791-731 Placerat. Ave Orléans","06 45 75 54 12");
+  ("Woodsen","Caroline","carowood@gmail.com","1999/03/17",62,"3 rue du pendu Orléans",06 32 71 33 08),
+  ("Warldoff","Elena","elanwarl@gmail.com","1992/10/01",59,"45 rue perdu Orléans",07 57 71 94 22),
+  ("Salvator","Emilien","emilensalvator1@outlook.fr","1983/01/25",75,"5bis allée de l'impasse",07 48 27 33 01)
+  
 
-  ("Payne","Chanda","erat.neque@protonmail.net","2022/02/22",37,"5828 Vel, Av. Orléans","02 40 44 84 36"),
-  ("Franklin","Gil","taciti.sociosqu@outlook.edu","2003/06/13",37,"P.O. Box 739, 3227 Eleifend, St. Orléans","09 66 19 08 11"),
-  ("Sharp","Stuart","tincidunt@hotmail.ca","2004/12/08",44,"2867 Varius Av. Orléans","04 47 15 16 64"),
-  ("Rowe","Tara","vel.est.tempor@yahoo.org","2004/06/23",41,"Ap #813-6790 Imperdiet Road Orléans","02 11 94 65 65"),
-  ("Alexander","Hiroko","ridiculus@icloud.couk","2015/12/26",42,"Ap #606-3538 Venenatis St. Orléans","03 85 65 96 04"),
-  ("Flynn","Teegan","sagittis.semper@aol.ca","2007/05/11",31,"Ap #956-5843 Sem, Street Orléans","02 68 66 11 82"),
-  ("Lara","Hilda","sit.amet@yahoo.edu","2011/07/18",26,"P.O. Box 100, 3902 Fermentum St. Orléans","07 76 38 56 83"),
-  ("Sims","Brandon","vestibulum@aol.com","2016/10/13",36,"P.O. Box 650, 882 Cursus Avenue Orléans","04 63 33 22 86"),
-  ("Alexander","Daria","nunc@outlook.ca","2021/10/23",41,"Ap #509-1030 Pellentesque Av. Orléans","02 48 27 88 53"),
-  ("Perez","Prescott","cursus.nunc@outlook.ca","2000/07/16",31,"625-8947 Est. Av. Orléans","04 84 82 49 30");
+  ("Harrell","Nathaniel","ac.ipsum.phasellus@outlook.edu",STR_TO_DATE("2019/05/30", "%Y/%m/%d"),31,"724-8442 Egestas. Av. Orléans",04 46 67 12 86),
+  ("Vega","Channing","dolor.vitae.dolor@outlook.com",STR_TO_DATE("2024/02/07", "%Y/%m/%d"),38,"718-4464 Vivamus Rd. Orléans",05 41 43 37 16),
+  ("Montoya","Dante","suspendisse.sed@yahoo.couk",STR_TO_DATE("2011/03/28", "%Y/%m/%d"),40,"P.O. Box 801, 2753 Ornare Av. Orléans",08 38 08 00 68),
+  ("Horn","Camille","habitant.morbi.tristique@protonmail.couk",STR_TO_DATE("2007/04/21", "%Y/%m/%d"),25,"796-2339 Aliquam St. Orléans",04 66 43 25 18),
+  ("King","Cassady","rutrum@google.couk",STR_TO_DATE("2006/01/04", "%Y/%m/%d"),25,"163-2249 Diam Ave Orléans",03 67 04 22 81),
+  ("Haley","Jana","dolor.quisque@outlook.couk",STR_TO_DATE("2021/01/23", "%Y/%m/%d"),26,"P.O. Box 611, 7785 Tincidunt Av. Orléans",08 37 44 87 63),
+  ("Hewitt","Hyatt","massa@aol.ca",STR_TO_DATE("2002/01/17", "%Y/%m/%d"),23,"P.O. Box 431, 858 Nulla Rd. Orléans",08 52 88 33 80),
+  ("Bonner","Joel","non@protonmail.org",STR_TO_DATE("2002/07/21", "%Y/%m/%d"),21,"188-4693 Nam St. Orléans",04 71 54 71 35),
+  ("Burgess","Rashad","in.aliquet@icloud.net",STR_TO_DATE("2002/11/28", "%Y/%m/%d"),43,"Ap #384-5918 Nulla Avenue Orléans",08 10 42 27 83),
+  ("Stark","Serena","varius.et@protonmail.net",STR_TO_DATE("2020/07/22", "%Y/%m/%d"),39,"Ap #791-731 Placerat. Ave Orléans",06 45 75 54 12),
 
-  ("Blackwell","Kennedy","odio.aliquam.vulputate@google.edu","2003/08/15",39,"Ap #985-6107 Sem St. Orléans","01 88 53 42 11"),
-  ("Butler","Tiger","in.ornare@aol.com","2008/08/09",28,"Ap #762-5007 Felis. Street Orléans","04 15 37 42 61"),
-  ("York","David","tellus.aenean@aol.net","2010/01/08",46,"551-6219 Donec Road Orléans","05 35 44 61 37"),
-  ("Castillo","Kameko","luctus@icloud.ca","2000/08/09",35,"471-6917 Per St. Orléans","02 31 14 83 09"),
-  ("Burnett","Gisela","ut.sagittis@aol.ca","2007/04/20",31,"Ap #678-6021 Nunc Street Orléans","05 49 27 77 53"),
-  ("Craft","Castor","nunc.interdum@aol.ca","2010/04/01",22,"Ap #194-2936 Risus. St. Orléans","05 77 48 22 77"),
-  ("Cunningham","Guy","integer@outlook.couk","2000/10/17",22,"694-4152 Nulla Ave Orléans","07 13 67 08 22"),
-  ("Harris","Graiden","aliquam.ultrices@icloud.edu","2013/04/17",21,"8259 Sed Rd. Orléans","04 35 48 85 24");
+  ("Payne","Chanda","erat.neque@protonmail.net",STR_TO_DATE("2022/02/22", "%Y/%m/%d"),37,"5828 Vel, Av. Orléans",02 40 44 84 36),
+  ("Franklin","Gil","taciti.sociosqu@outlook.edu",STR_TO_DATE("2003/06/13", "%Y/%m/%d"),37,"P.O. Box 739, 3227 Eleifend, St. Orléans",09 66 19 08 11),
+  ("Sharp","Stuart","tincidunt@hotmail.ca",STR_TO_DATE("2004/12/08", "%Y/%m/%d"),44,"2867 Varius Av. Orléans",04 47 15 16 64),
+  ("Rowe","Tara","vel.est.tempor@yahoo.org",STR_TO_DATE("2004/06/23", "%Y/%m/%d"),41,"Ap #813-6790 Imperdiet Road Orléans",02 11 94 65 65),
+  ("Alexander","Hiroko","ridiculus@icloud.couk",STR_TO_DATE("2015/12/26", "%Y/%m/%d"),42,"Ap #606-3538 Venenatis St. Orléans",03 85 65 96 04),
+  ("Flynn","Teegan","sagittis.semper@aol.ca",STR_TO_DATE("2007/05/11", "%Y/%m/%d"),31,"Ap #956-5843 Sem, Street Orléans",02 68 66 11 82),
+  ("Lara","Hilda","sit.amet@yahoo.edu",STR_TO_DATE("2011/07/18", "%Y/%m/%d"),26,"P.O. Box 100, 3902 Fermentum St. Orléans",07 76 38 56 83),
+  ("Sims","Brandon","vestibulum@aol.com",STR_TO_DATE("2016/10/13", "%Y/%m/%d"),36,"P.O. Box 650, 882 Cursus Avenue Orléans",04 63 33 22 86),
+  ("Alexander","Daria","nunc@outlook.ca",STR_TO_DATE("2021/10/23", "%Y/%m/%d"),41,"Ap #509-1030 Pellentesque Av. Orléans",02 48 27 88 53),
+  ("Perez","Prescott","cursus.nunc@outlook.ca",STR_TO_DATE("2000/07/16", "%Y/%m/%d"),31,"625-8947 Est. Av. Orléans",04 84 82 49 30);
+
+  ("Blackwell","Kennedy","odio.aliquam.vulputate@google.edu",STR_TO_DATE("2003/08/15", "%Y/%m/%d"),39,"Ap #985-6107 Sem St. Orléans",01 88 53 42 11),
+  ("Butler","Tiger","in.ornare@aol.com",STR_TO_DATE("2008/08/09", "%Y/%m/%d"),28,"Ap #762-5007 Felis. Street Orléans",04 15 37 42 61),
+  ("York","David","tellus.aenean@aol.net",STR_TO_DATE("2010/01/08", "%Y/%m/%d"),46,"551-6219 Donec Road Orléans",05 35 44 61 37),
+  ("Castillo","Kameko","luctus@icloud.ca",STR_TO_DATE("2000/08/09", "%Y/%m/%d"),35,"471-6917 Per St. Orléans",02 31 14 83 09),
+  ("Burnett","Gisela","ut.sagittis@aol.ca",STR_TO_DATE("2007/04/20", "%Y/%m/%d"),31,"Ap #678-6021 Nunc Street Orléans",05 49 27 77 53),
+  ("Craft","Castor","nunc.interdum@aol.ca",STR_TO_DATE("2010/04/01", "%Y/%m/%d"),22,"Ap #194-2936 Risus. St. Orléans",05 77 48 22 77),
+  ("Cunningham","Guy","integer@outlook.couk",STR_TO_DATE("2000/10/17", "%Y/%m/%d"),22,"694-4152 Nulla Ave Orléans",07 13 67 08 22),
+  ("Harris","Graiden","aliquam.ultrices@icloud.edu",STR_TO_DATE("2013/04/17", "%Y/%m/%d"),21,"8259 Sed Rd. Orléans",04 35 48 85 24);
 
 
 
@@ -57,5 +62,19 @@ INSERT INTO MONITEUR values (10,"CDD",STR_TO_DATE("11/02/2024", "%d/%m/%Y"));
 INSERT INTO MONITEUR values (11,"CDD",STR_TO_DATE("22/03/2024", "%d/%m/%Y"));
 INSERT INTO MONITEUR values (10,"CDI",STR_TO_DATE("15/01/2024", "%d/%m/%Y"));
 
-Insert into COURS (NOMCOURS, DUREE, TYPEC) values ()
+Insert into COURS (NOMCOURS, DUREE, TYPEC) values ('Dressage', 1, 2), 
+('Dressage', 1.5, 1),
+('Attelage', 1.1, 1),
+('Endurance', 1.5, 1),
+('CSO', 2, 1),
+('CSO', 1.5, 2),
+('Groom', 2, 2);
+
+Insert into PONEY (IDPO, NOMPO, DDNPO, POIDS_MAX, RACE, GENRE) values 
+('Didi', STR_TO_DATE("12/12/2018", "%d/%m/%Y"), 46, 'Shetland', 1),
+('Patrick', STR_TO_DATE("28/02/2017", "%d/%m/%Y"), 51, 'Shetland', 2),
+('Tituan', STR_TO_DATE("23/06/2015", "%d/%m/%Y"), 45, 'Fjord', 2),
+('Sophie', STR_TO_DATE("31/10/2018", "%d/%m/%Y"), 46, 'Fjord', 1),
+('Michelle', STR_TO_DATE("17/07/2016", "%d/%m/%Y"), 46, 'Shetland', 1);
+
 
