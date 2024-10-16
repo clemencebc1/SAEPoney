a492c08d80f85b51c7aa3a259b126735c27df124
Clemence <clemence.bcq@gmail.com~>
2024-10-16 09:40:30 +0200

    modification script

---
+++ Database/script/dataPersonnes.sql
@@ -0,0 +1,84 @@
+-- Générateur source : https://generatedata.com/-- pour les insertions de PERSONNE
+
+INSERT INTO PERSONNE (IDPER,NOMPER, PRENOMPER, EMAIL, DDNPER, POIDS, ADRESSE, PORTABLE)
+VALUES
+  (1,"Perry","Macon","in@icloud.org",STR_TO_DATE("2010/09/18", "%Y/%m/%d"),20,"Ap #731-3409 Eu Road Orléans","03 36 78 33 08"),
+  (2,"Suarez","Cecilia","vivamus.non@google.edu",STR_TO_DATE("2000/01/14", "%Y/%m/%d"),29,"621 Etiam Av. Orléans","05 05 21 22 81"),
+  (3,"Morris","Brielle","semper@yahoo.ca",STR_TO_DATE("2019/06/05", "%Y/%m/%d"),31,"819-3025 Lacus. Rd. Orléans","06 82 05 38 68"),
+  (4,"Mccarty","Stuart","adipiscing.lobortis.risus@outlook.couk",STR_TO_DATE("2021/11/23", "%Y/%m/%d"),45,"2671 Tincidunt Avenue Orléans","03 63 57 85 20"),
+  (5,"Marsh","Brielle","faucibus.orci@icloud.org",STR_TO_DATE("2014/08/18", "%Y/%m/%d"),49,"P.O. Box 753, 1151 Cursus Rd. Orléans","06 31 23 50 66"),
+  (6,"Davenport","Raymond","ante.dictum@hotmail.com",STR_TO_DATE("2013/01/25", "%Y/%m/%d"),23,"471-5249 Ligula Rd. Orléans","07 43 74 15 75"),
+  (7,"Cruz","Eaton","laoreet@outlook.net",STR_TO_DATE("2008/09/09", "%Y/%m/%d"),38,"5649 Vitae Rd. Orléans","06 20 43 68 42"),
+  (8,"Franklin","Nicole","congue@google.couk",STR_TO_DATE("2007/05/20", "%Y/%m/%d"),33,"P.O. Box 495, 4960 Curabitur St. Orléans","03 08 68 41 41"),
+  (9,"Barrett","Clinton","interdum@icloud.edu",STR_TO_DATE("2022/03/08", "%Y/%m/%d"),45,"Ap #651-718 Sed St. Orléans","04 93 55 61 23"),
+  (10,"Houston","Basia","penatibus@aol.edu",STR_TO_DATE("2013/08/03", "%Y/%m/%d"),32,"888-3365 Neque. St. Orléans","09 31 64 22 54"),
+
+  (11,"Woodsen","Caroline","carowood@gmail.com","1999/03/17",62,"3 rue du pendu Orléans","06 32 71 33 08"),
+  (12,"Warldoff","Elena","elanwarl@gmail.com","1992/10/01",59,"45 rue perdu Orléans","07 57 71 94 22"),
+  (13,"Salvator","Emilien","emilensalvator1@outlook.fr","1983/01/25",75,"5bis allée de l'impasse","07 48 27 33 01"),
+  
+
+  (14,"Harrell","Nathaniel","ac.ipsum.phasellus@outlook.edu",STR_TO_DATE("2019/05/30", "%Y/%m/%d"),31,"724-8442 Egestas. Av. Orléans","04 46 67 12 86"),
+  (15,"Vega","Channing","dolor.vitae.dolor@outlook.com",STR_TO_DATE("2024/02/07", "%Y/%m/%d"),38,"718-4464 Vivamus Rd. Orléans","05 41 43 37 16"),
+  (16,"Montoya","Dante","suspendisse.sed@yahoo.couk",STR_TO_DATE("2011/03/28", "%Y/%m/%d"),40,"P.O. Box 801, 2753 Ornare Av. Orléans","08 38 08 00 68"),
+  (17,"Horn","Camille","habitant.morbi.tristique@protonmail.couk",STR_TO_DATE("2007/04/21", "%Y/%m/%d"),25,"796-2339 Aliquam St. Orléans","04 66 43 25 18"),
+  (18,"King","Cassady","rutrum@google.couk",STR_TO_DATE("2006/01/04", "%Y/%m/%d"),25,"163-2249 Diam Ave Orléans","03 67 04 22 81"),
+  (19,"Haley","Jana","dolor.quisque@outlook.couk",STR_TO_DATE("2021/01/23", "%Y/%m/%d"),26,"P.O. Box 611, 7785 Tincidunt Av. Orléans","08 37 44 87 63"),
+  (20,"Hewitt","Hyatt","massa@aol.ca",STR_TO_DATE("2002/01/17", "%Y/%m/%d"),23,"P.O. Box 431, 858 Nulla Rd. Orléans","08 52 88 33 80"),
+  (21,"Bonner","Joel","non@protonmail.org",STR_TO_DATE("2002/07/21", "%Y/%m/%d"),21,"188-4693 Nam St. Orléans","04 71 54 71 35"),
+  (22,"Burgess","Rashad","in.aliquet@icloud.net",STR_TO_DATE("2002/11/28", "%Y/%m/%d"),43,"Ap #384-5918 Nulla Avenue Orléans","08 10 42 27 83"),
+  (23,"Stark","Serena","varius.et@protonmail.net",STR_TO_DATE("2020/07/22", "%Y/%m/%d"),39,"Ap #791-731 Placerat. Ave Orléans","06 45 75 54 12"),
+
+  (24,"Payne","Chanda","erat.neque@protonmail.net",STR_TO_DATE("2022/02/22", "%Y/%m/%d"),37,"5828 Vel, Av. Orléans","02 40 44 84 36"),
+  (25,"Franklin","Gil","taciti.sociosqu@outlook.edu",STR_TO_DATE("2003/06/13", "%Y/%m/%d"),37,"P.O. Box 739, 3227 Eleifend, St. Orléans","09 66 19 08 11"),
+  (26,"Sharp","Stuart","tincidunt@hotmail.ca",STR_TO_DATE("2004/12/08", "%Y/%m/%d"),44,"2867 Varius Av. Orléans","04 47 15 16 64"),
+  (27,"Rowe","Tara","vel.est.tempor@yahoo.org",STR_TO_DATE("2004/06/23", "%Y/%m/%d"),41,"Ap #813-6790 Imperdiet Road Orléans","02 11 94 65 65"),
+  (28,"Alexander","Hiroko","ridiculus@icloud.couk",STR_TO_DATE("2015/12/26", "%Y/%m/%d"),42,"Ap #606-3538 Venenatis St. Orléans","03 85 65 96 04"),
+  (29,"Flynn","Teegan","sagittis.semper@aol.ca",STR_TO_DATE("2007/05/11", "%Y/%m/%d"),31,"Ap #956-5843 Sem, Street Orléans","02 68 66 11 82"),
+  (30,"Lara","Hilda","sit.amet@yahoo.edu",STR_TO_DATE("2011/07/18", "%Y/%m/%d"),26,"P.O. Box 100, 3902 Fermentum St. Orléans","07 76 38 56 83"),
+  (31,"Sims","Brandon","vestibulum@aol.com",STR_TO_DATE("2016/10/13", "%Y/%m/%d"),36,"P.O. Box 650, 882 Cursus Avenue Orléans","04 63 33 22 86"),
+  (32,"Alexander","Daria","nunc@outlook.ca",STR_TO_DATE("2021/10/23", "%Y/%m/%d"),41,"Ap #509-1030 Pellentesque Av. Orléans","02 48 27 88 53"),
+  (33,"Perez","Prescott","cursus.nunc@outlook.ca",STR_TO_DATE("2000/07/16", "%Y/%m/%d"),31,"625-8947 Est. Av. Orléans","04 84 82 49 30"),
+
+  (34,"Blackwell","Kennedy","odio.aliquam.vulputate@google.edu",STR_TO_DATE("2003/08/15", "%Y/%m/%d"),39,"Ap #985-6107 Sem St. Orléans","01 88 53 42 11"),
+  (35,"Butler","Tiger","in.ornare@aol.com",STR_TO_DATE("2008/08/09", "%Y/%m/%d"),28,"Ap #762-5007 Felis. Street Orléans","04 15 37 42 61"),
+  (36,"York","David","tellus.aenean@aol.net",STR_TO_DATE("2010/01/08", "%Y/%m/%d"),46,"551-6219 Donec Road Orléans","05 35 44 61 37"),
+  (37,"Castillo","Kameko","luctus@icloud.ca",STR_TO_DATE("2000/08/09", "%Y/%m/%d"),35,"471-6917 Per St. Orléans","02 31 14 83 09"),
+  (38,"Burnett","Gisela","ut.sagittis@aol.ca",STR_TO_DATE("2007/04/20", "%Y/%m/%d"),31,"Ap #678-6021 Nunc Street Orléans","05 49 27 77 53"),
+  (39,"Craft","Castor","nunc.interdum@aol.ca",STR_TO_DATE("2010/04/01", "%Y/%m/%d"),22,"Ap #194-2936 Risus. St. Orléans","05 77 48 22 77"),
+  (40,"Cunningham","Guy","integer@outlook.couk",STR_TO_DATE("2000/10/17", "%Y/%m/%d"),22,"694-4152 Nulla Ave Orléans","07 13 67 08 22"),
+  (41,"Harris","Graiden","aliquam.ultrices@icloud.edu",STR_TO_DATE("2013/04/17", "%Y/%m/%d"),21,"8259 Sed Rd. Orléans","04 35 48 85 24");
+
+
+
+INSERT INTO ADHERENT values (1,STR_TO_DATE("14/05/2025", "%d/%m/%Y"),2);
+INSERT INTO ADHERENT values (2,STR_TO_DATE("17/05/2025", "%d/%m/%Y"),1);
+INSERT INTO ADHERENT values (3,STR_TO_DATE("20/05/2025", "%d/%m/%Y"),1);
+INSERT INTO ADHERENT values (4,STR_TO_DATE("04/06/2025", "%d/%m/%Y"),2);
+INSERT INTO ADHERENT values (5,STR_TO_DATE("05/06/2025", "%d/%m/%Y"),1);
+INSERT INTO ADHERENT values (6,STR_TO_DATE("11/06/2025", "%d/%m/%Y"),1);
+
+INSERT INTO MONITEUR values (11,"CDD",STR_TO_DATE("11/02/2024", "%d/%m/%Y"));
+INSERT INTO MONITEUR values (12,"CDD",STR_TO_DATE("22/03/2024", "%d/%m/%Y"));
+INSERT INTO MONITEUR values (13,"CDI",STR_TO_DATE("15/01/2024", "%d/%m/%Y"));
+
+Insert into COURS (NUMCOURS,NOMCOURS, TYPEC) values (0,'Dressage', 2), 
+(1,'Dressage', 1),
+(2,'Attelage', 1),
+(3,'Endurance', 1),
+(4,'CSO', 1),
+(5,'CSO', 2),
+(6,'Groom', 2);
+
+
+
+Insert into PONEY (NOMPO, DDNPO, POIDS_MAX, RACE, SEXE) values 
+('Didi', STR_TO_DATE("12/12/2018", "%d/%m/%Y"), 46, 'Shetland', 1),
+('Patrick', STR_TO_DATE("28/02/2017", "%d/%m/%Y"), 51, 'Shetland', 2),
+('Tituan', STR_TO_DATE("23/06/2015", "%d/%m/%Y"), 45, 'Fjord', 2),
+('Sophie', STR_TO_DATE("31/10/2018", "%d/%m/%Y"), 46, 'Fjord', 1),
+('Michelle', STR_TO_DATE("17/07/2016", "%d/%m/%Y"), 46, 'Shetland', 1);
+
+Insert into SEANCE (IDSEANCE, PRIX, DUREE, NIVEAU, DESCRIPTIF, GROUPE_AGE, NUMCOURS, DATE_SEANCE) 
+values (1, 25, 1.1, 1, "baby poney", "moins de 6 ans", 6, STR_TO_DATE("16/10/2024:14/10","%d/%m/%Y:%H/%i")),
+(2, 35, 1.2,1, "baby grand poney", "plus de 6 ans - moins de 10 ans", 6,STR_TO_DATE("16/10/2024:10/00","%d/%m/%Y:%H/%i"));
--- Database/script/procedures.sql
+++ Database/script/procedures.sql
@@ -1,5 +1,6 @@
 -- script drop procedure 
 DROP TRIGGER IF EXISTS addPersonneCours;
+DROP TRIGGER IF EXISTS poids_max_poneys
 -- Verification qu'il n'y a pas plus de 1. personne dans le cours
 delimiter |
 CREATE OR REPLACE TRIGGER addPersonneCours BEFORE INSERT ON PARTICIPER FOR EACH ROW
@@ -14,4 +15,47 @@ BEGIN
     END IF ;
 END |
 DELIMITER ;
-    
+ 
+
+
+-- les poneys ne peuvent pas supporter plus d'un certain poids
+delimiter |
+create or replace trigger poids_max_poneys before insert on PARTICIPER for each ROW
+BEGIN
+    declare poids_poney int;
+    declare mes varchar(100);
+    declare poids_adherent int;
+    select POIDS_MAX into poids_poney from PONEY where IDPO = new.IDPO;
+    select POIDS into poids_adherent from PERSONNE natural join ADHERENT where IDADH = new.IDADH;
+    if poids_poney < poids_adherent THEN
+        set mes = concat(mes, 'Le poney ne peut pas porter plus de ', poids_poney, ' kg');
+        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
+    end if;
+end |
+delimiter ;
+
+-- une seance payee ne peut plus passer a false
+delimiter |
+create or replace trigger paye before update on PARTICIPER for each Row
+begin 
+    declare paye_s boolean;
+    declare mess varchar(50) default "seance deja payee";
+    select PAYE into paye_s from PARTICIPER where NUMCOURS = new.NUMCOURS and IDSEANCE = new.IDSEANCE and DATEPART = new.DATEPART and IDADH = new.IDADH;
+    if paye_s THEN
+            signal SQLSTATE '45000' set MESSAGE_TEXT = mess;
+    end if;
+end |
+
+-- un poney doit avoir au minimm 1 de repos entre chaque cours
+delimiter |
+create or replace trigger repos before insert on PARTICIPER for each Row
+begin
+    declare date_cours DATETIME;
+    declare mes varchar(50) default "le poney a besoin de repos";
+    select DATEPART into date_cours from PARTICIPER where IDPO = new.IDPO and DAY(DATEPART) = DAY(new.DATEPART) and MONTH(DATEPART) = MONTH(new.DATEPART) and YEAR(DATEPART) = YEAR(new.DATEPART);
+    if DATEDIFF(new.DATEPART, date_cours)<1 THEN
+        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
+    end if;
+end |
+
+
