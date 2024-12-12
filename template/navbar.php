5d980b9516a4a942df0dedbc1958d47a60483849
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-12-12 15:48:36 +0100

    ajout du template de la nav bar (header)

---
+++ template/navbar.php
@@ -0,0 +1,24 @@
+<html>
+    <header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Logo</a></li>
+            <li><a href="#tarifs">Nos tarifs</a></li>
+            <li><a href="#planning">Notre planning</a></li>
+            <li><a href="#poneys">Nos poney</a></li>
+            <li><a href="#news">Actualites</a></li>
+            <li><a href="#testimony">Temoignages</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <!-- dans le future utiliser php pour coder le faite que si l'user est co n afficher pas les boutton  -->
+            <li><a href="#login">Se connecter</a></li>
+            <li><a href="#register">S'inscrire</a></li>
+        </ul>
+    </nav>
+    <i></i>
+</header>
+</html>
+
+<?php
+
+
+?> 
