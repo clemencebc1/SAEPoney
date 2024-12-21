d21eccc19509a64f76886ba838d20c2379062280
clemence <clemence.bcq@gmail.com>
2024-12-21 18:14:54 +0100

    mise a jour de l'arborescence et heritage de html

---
+++ templates/global/footer.php
@@ -0,0 +1,10 @@
+<style>
+    footer {
+    text-align: center;
+    border: thick solid lightgray;
+    margin-top: auto;
+}
+</style>
+<footer>
+    <p>Clémence Bocquet - Ophélie Valin - Nathan Randriantsoa</p>
+</footer>
---
+++ templates/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poney Club</title>
+    <link rel="stylesheet" href="static/accueil.css">
+</head>
+<body>
+    <?php include('global/header.php');  ?>
+    <main>
+        <div class="poney">img here</div>
+        <div class="content">
+            <div class="box-img"><img src="img/poney-2.jpg" alt="Photo de poney libre de droits"></div>
+            <div class="box">Grand Galop, un poney club de Sologne et adhérente à la Fédération Française de l'équitation, a le plaisir le vous accueillir dans nos centres équestres. Nous vous proposons de découvrir nos poneys, notre planning d'activité et l'avis de nos adhérents. </div>
+            <div class="box deuxieme-ligne">Plus d'informations</div>
+        </div>
+    </main>
+    <?php include('global/footer.php') ?>
+</body>
+</html>
