87baf9d2d7cbb532da9c6835778483a57fa2f64b
clemence <clemence.bcq@gmail.com>
2024-12-24 20:31:24 +0100

    modification du footer conforme à la maquette

--- templates/global/footer.php
+++ templates/global/footer.php
@@ -1,10 +1,48 @@
 <style>
-    footer {
-    text-align: center;
-    border: thick solid lightgray;
-    margin-top: auto;
-}
+
 </style>
 <footer>
-    <p>Clémence Bocquet - Ophélie Valin - Nathan Randriantsoa - BUT2 Informatique IUT Orléans</p>
+    <div class="media">
+        <div id="espace-logo">
+            <img src="logo.png" alt="Logo" class="logo">
+            <div id="social-icons">
+                <a href="https://x.com"><img src="img/icon-social-media/twitter.png" alt="X"></a>
+                <a href="https://www.instagram.com"><img src="img/icon-social-media/instagram.png" alt="Instagram"></a>
+                <a href="https://www.youtube.com"><img src="img/icon-social-media/youtube.png" alt="YouTube"></a>
+            </div>
+        </div>
+
+        <ul class="liste">
+            <li class="colonne">
+                <h3>Nous contacter</h3>
+                <ul>
+                    <li><a href="tel:+33102030405">Tel : +33 1 02 03 04 05</a></li>
+                    <li><a href="mailto:grandgalop@mail.com">Email : Notre grandgalop@mail.com/a></li>
+                    <li><a href="https://www.google.com/maps">Adresse : 2 rue du grand poney, Sologne</a></li>
+                    <li><a href="#">Grand Galop</a></li>
+                    <li><a href="#">Formulaire</a></li>
+                </ul>
+            </li>
+            <li class="colonne">
+                <h3>À propos de nous</h3>
+                <ul>
+                    <li><a href="#">Tarifs</a></li>
+                    <li><a href="#">Planning</a></li>
+                    <li><a href="#">Poneys</a></li>
+                    <li><a href="#">Témoignages</a></li>
+                </ul>
+            </li>
+            <li class="colonne">
+                <h3>Compte</h3>
+                <ul>
+                    <li><a href="connexion.php">Se connecter</a></li>
+                    <li><a href="#">Centre d'aide</a></li>
+                    <li><a href="#">Support</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div id="mention">
+        <p>Mentions légales et conditions d’utilisations</p>
+    </div>
 </footer>
