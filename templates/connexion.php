4caf9e63faf6da5186ee8d57bde8104d1ac20380
clemence <clemence.bcq@gmail.com>
2024-12-24 19:49:07 +0100

    ajout du onclick poour le bouton de connexion

--- templates/connexion.php
+++ templates/connexion.php
@@ -2,7 +2,9 @@
 <html lang="fr">
 <?php include 'global/head.php'; 
 title_html('Connexion');
-link_to_css('static/connexion.css'); ?>
+link_to_css('static/connexion.css');
+
+?>
 <body>
     <?php include('global/header.php') ?>
     <main>
@@ -16,7 +18,7 @@ link_to_css('static/connexion.css'); ?>
                     <label for="password">Mot de passe</label>
                     <input type="password" id="password" placeholder="Votre mot de passe" required>
                     
-                    <button type="submit" id="login-btn">Se connecter</button>
+                    <button type="submit" id="login-btn" onclick="" >Se connecter</button>
                 </form>
                 <p class="inscrire">Pas de compte ? <a href="inscription.php">Inscrivez-vous !</a></p>
             </div>
