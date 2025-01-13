7869bca7257343a10ea75f72da7bf3caec3cc329
Clemence <clemence.bcq@gmail.com~>
2025-01-13 10:32:26 +0100

    page niveau et mise a jour facture

--- templates/index_connected.php
+++ templates/index_connected.php
@@ -23,7 +23,7 @@ UserTools::requireLogin();
                     <?php $cours_user = $connexion->get_seances_for_user('in@icloud.org'); 
                         $compteur = 0;
                     ?>
-                     Votre prochain cours a lieu le <strong><?php echo $cours_user[$compteur] ?></strong></p>
+                     Votre prochain cours a lieu le <strong><?php echo $cours_user[$compteur]["DATE_SEANCE"] ?></strong></p>
                 <p class="cours-details"><?php echo $cours_user[1] ?></p>
                 <button class="btn-voir-cours"><a href="planning-adherent.php">Voir tous mes cours</a></button>
             </div>
@@ -32,10 +32,18 @@ UserTools::requireLogin();
         <section class="block-facture">
             <div id="info">
                 <div class="clock-icon">
-                    <img src="clock-icon.png" alt="Clock">
+                    <img src="img/icon-clock.jpg" alt="Clock">
                 </div>
-                <p id="days">3 jours</p>
-                <p id="days-description">Facture à payer</p>
+                <?php $factures = $connexion->get_factures_user($_SESSION['user']['username']);
+                foreach ($factures as $facture){
+                    if (!($facture["PAYE"])){
+                        echo "<p id='days'>3 jours</p>
+                <p id='days-description'>Facture à payer</p>";
+                break;
+                    }
+                    else{ echo "<p id='days'> Aucune facture </p>";
+                break;}
+                }?>
             </div>
         </section>
     </main>
